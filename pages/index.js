d91609aaf793d6352e864069f2531fcd5af528d4
Fabio Rodrigues da Silva <frsilva2101@gmail.com>
2022-09-04 22:48:06 -0300

    Tentando configurar o styles com bootstrap, incompleto

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,35 @@
+import styles from '../style/styles.module.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Container, Row, Col, Card, CardBody, Form, Label, Input, CardText } from 'reactstrap';
+
+
+
 export default function Home() {
   return (
-    <div>
-      <div>
-        <h1>Registre-se</h1>
-        <form action="/api/register" method="post">
-          <label> Endereço de e-mail</label><br />
-          <input type='email' name='email' placeholder='Escreva seu e-mail'></input><br /><br />
+    <Row className={styles.principal}>
+      <Col class="col-sm-6">
+        <Container className={styles.CardOne}>
+          <h1>Registre-se</h1>
+          <Form action="/api/register" method="post">
+            <Input type='email' name='email' placeholder='Escreva seu e-mail'></Input><br /><br />
+            <Input type='text' name='text' placeholder='Digite seu nome'></Input><br /><br />
+            <Input type='password' name='password' placeholder='Digite sua senha'></Input><br /><br />
+            <Button type='submit'>Registrar</Button>
+          </Form>
+        </Container>
 
-          <label>Nome de usuário</label><br />
-          <input type='text' name='text' placeholder='Digite seu nome'></input><br /><br />
+      </Col>
+        <Col class="col-sm-6">
+          <Container className={styles.CardTwo}>
+                <h1>Login</h1>
+              <Form action="/api/login" method="post">
+                <Input type='email' name='email' placeholder='Digite seu e-mail'></Input><br /><br />
+                <Input type='password' name='password' placeholder='Digite sua senha'></Input><br /><br />
+                <Button type='submit' color='success' size='md'>ENTRAR</Button>
 
-          <label>Senha</label><br />
-          <input type='password' name='password' placeholder='Digite sua senha'></input><br /><br />
-
-          <input type='submit' value='Registrar-se'></input>
-        </form>
-      </div>
-
-      <div>
-        <h1>Login</h1>
-        <form action="/api/login" method="post">
-          <label>Endereço de e-mail</label><br />
-          <input type='email' name='email' placeholder='Digite seu e-mail'></input><br /><br />
-
-          <label>Senha</label><br />
-          <input type='password' name='password' placeholder='Digite sua senha'></input><br /><br />
-
-          <input type='submit' value='Login'></input>
-
-        </form>
-      </div>
-    </div>
+              </Form>
+          </Container>
+        </Col>
+    </Row>
   )
 }
